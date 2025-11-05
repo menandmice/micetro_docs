cf85a1e942ea9d6593c4b673ffc6627822d55650
rachelbritton <rbritton@bluecatnetworks.com>
2025-11-05 14:25:34 +0000

    Fixed formatting.
    Adjusted CSS so the banner is formatted correctly on all viewport sizes.

--- _static/custom.js
+++ _static/custom.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const banner = document.createElement("div");
-    banner.innerHTML = 'The latest documentation for Micetro is now available on the <a href="https://docs.bluecatnetworks.com/">BlueCat Documentation Portal</a>, where you can access the entire catalog of BlueCat product documentation. This documentation site will not be actively maintained. Please update any relevant bookmarks to point to the documentation available on the BlueCat Documentation Portal.';
-    banner.style.position = "relative";
-    banner.style.background = "#ffcc00";
-    banner.style.color = "#000";
-    banner.style.left = "300px"
-    banner.style.textAlign = "center";
-    banner.style.padding = "20px";
-    banner.style.width = "80%"
-    banner.style.fontWeight = "bold";
-    document.body.insertBefore(banner, document.body.firstChild);
+const banner = document.createElement("div");
+banner.innerHTML = 'The latest documentation for Micetro is now available on the <a href="https://docs.bluecatnetworks.com/">BlueCat Documentation Portal</a>, where you can access the entire catalog of BlueCat product documentation. This documentation site will not be actively maintained. Please update any relevant bookmarks to point to the documentation available on the BlueCat Documentation Portal.';
+banner.style.position = "relative";
+banner.style.background = "#ffcc00";
+banner.style.color = "#000";
+banner.style.textAlign = "center";
+banner.style.padding = "20px";
+banner.style.margin = "0 auto"; // center horizontally
+banner.style.fontWeight = "bold";
+banner.style.boxSizing = "border-box";
+document.body.insertBefore(banner, document.body.firstChild);
+const target = document.querySelector('.wy-nav-content');
+if (target && target.parentNode) {
+  target.parentNode.insertBefore(banner, target);
+}
 });
